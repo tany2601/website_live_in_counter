71762d2df97112dbfe64e1079118bc32e5c6093a
Thanay <thanayab08@gmail.com>
2024-08-09 10:54:07 +0530

    counter

---
+++ src/App.jsx
@@ -0,0 +1,105 @@
+import { useAnimate } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
+import './style.css';
+
+
+const COUNTDOWN_FROM = "2024-08-10";
+
+const SECOND = 1000;
+const MINUTE = SECOND * 60;
+const HOUR = MINUTE * 60;
+const DAY = HOUR * 24;
+
+const ShiftingCountdown = () => {
+  return (
+
+      
+    
+    <div className="counter-div min-h-screen flex bg-gradient-to-br from-violet-600 to-indigo-600 p-4">
+      <h1>OUR WEBSITE WILL BE LIVE IN</h1>
+      <div className="mx-auto h-full flex w-full max-w-5xl items-center bg-white">
+        <CountdownItem unit="Day" text="days" />
+        <CountdownItem unit="Hour" text="hours" />
+        <CountdownItem unit="Minute" text="minutes" />
+        <CountdownItem unit="Second" text="seconds" />
+      </div>
+      <h1 className="text">  STAY TUNED!!</h1>
+    </div>
+    );
+};
+
+const CountdownItem = ({ unit, text }) => {
+  const { ref, time } = useTimer(unit);
+
+  return (
+    <div className="flex h-24 w-1/4 flex-col items-center justify-center gap-1 border-r-[1px] border-slate-200 font-mono md:h-36 md:gap-2">
+      <div className="relative w-full overflow-hidden text-center">
+        <span
+          ref={ref}
+          className="block text-2xl font-medium text-black md:text-4xl lg:text-6xl xl:text-7xl"
+        >
+          {time}
+        </span>
+      </div>
+      <span className="text-xs font-light text-slate-500 md:text-sm lg:text-base">
+        {text}
+      </span>
+    </div>
+  );
+};
+
+export default ShiftingCountdown;
+
+const useTimer = (unit) => {
+  const [ref, animate] = useAnimate();
+
+  const intervalRef = useRef(null);
+  const timeRef = useRef(0);
+
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    intervalRef.current = setInterval(handleCountdown, 1000);
+
+    return () => clearInterval(intervalRef.current || undefined);
+  }, []);
+
+  const handleCountdown = async () => {
+    const end = new Date(COUNTDOWN_FROM);
+    const now = new Date();
+    const distance = +end - +now;
+
+    let newTime = 0;
+
+    if (unit === "Day") {
+      newTime = Math.floor(distance / DAY);
+    } else if (unit === "Hour") {
+      newTime = Math.floor((distance % DAY) / HOUR);
+    } else if (unit === "Minute") {
+      newTime = Math.floor((distance % HOUR) / MINUTE);
+    } else {
+      newTime = Math.floor((distance % MINUTE) / SECOND);
+    }
+
+    if (newTime !== timeRef.current) {
+      // Exit animation
+      await animate(
+        ref.current,
+        { y: ["0%", "-50%"], opacity: [1, 0] },
+        { duration: 0.35 }
+      );
+
+      timeRef.current = newTime;
+      setTime(newTime);
+
+      // Enter animation
+      await animate(
+        ref.current,
+        { y: ["50%", "0%"], opacity: [0, 1] },
+        { duration: 0.35 }
+      );
+    }
+  };
+
+  return { ref, time };
+};
